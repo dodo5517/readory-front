ba3ad707590fdbaf0b5fd3a1e245aa38770674a0
dodo5517 <kh9015548@gmail.com>
2025-12-16 16:56:07 +0900

    feat: API_BASE_URL 환경변수 추가

--- src/utils/fetchWithAuth.ts
+++ src/utils/fetchWithAuth.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = process.env.API_BASE_URL!;
 
 export async function fetchWithAuth(input: string, init: RequestInit = {}): Promise<Response> {
     const accessToken = localStorage.getItem('accessToken');
